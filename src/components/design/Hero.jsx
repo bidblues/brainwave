7cf1e025a6598baeacfa8342bbf210b1011cdffe
bidblues <bidgov4@gmail.com>
2024-09-27 12:56:06 +0530

    new

--- src/components/design/Hero.jsx
+++ src/components/design/Hero.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { MouseParallax } from "react-just-parallax";
-
 import PlusSvg from "../../assets/svg/PlusSvg";
 
 export const Gradient = () => {
